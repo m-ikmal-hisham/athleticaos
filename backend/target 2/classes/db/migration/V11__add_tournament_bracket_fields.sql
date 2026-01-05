7c4a3fd95480f21c13c67d4a77ff648d92e91b3b
m-ikmal-hisham <hisham_khair90@live.com.my>
2026-01-05 13:17:50 +0800

    chore: remove accidental build artifacts

--- backend/target 2/classes/db/migration/V11__add_tournament_bracket_fields.sql
+++
@@ -1,4 +0,0 @@
--- Add tournament bracket configuration fields
-ALTER TABLE tournaments ADD COLUMN format VARCHAR(50);
-ALTER TABLE tournaments ADD COLUMN number_of_pools INTEGER;
-ALTER TABLE tournaments ADD COLUMN has_placement_stages BOOLEAN DEFAULT false;
